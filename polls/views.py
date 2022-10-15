5b6afbeb1eeb5092fbab545ccd11f339c6706915
alfred <alfred.scott@protonmail.com>
2022-10-15 16:50:17 -0400

    part 3 complete

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,29 @@
 # polls/views.py
 
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, World - I am Django")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question": question
+    }
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request, question_id):
+    response = "Question %s Results."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote on Question %s." % question_id)
